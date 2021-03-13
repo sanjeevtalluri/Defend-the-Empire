5e518c29b241b3bf4dc37c71d41f73db31c41c08
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-03-13 16:30:52 +0530

    Implemented closest enemy script

--- Defend the Empire/Assets/Tower/TargetLocator.cs
+++ Defend the Empire/Assets/Tower/TargetLocator.cs
@@ -7,19 +7,59 @@ public class TargetLocator : MonoBehaviour
     [SerializeField]
     private Transform weapon;
     private Transform target;
+
+    private Enemy[] possibleTargets;
+
+    [SerializeField]
+    private ParticleSystem ballista;
+
+    private float range = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
-        target = FindObjectOfType<EnemyMover>().transform;
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {    
+        FindCloserTarget();
         AimWeapon();
     }
     void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+        if (targetDistance < range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
         weapon.LookAt(target);
     }
+    void FindCloserTarget()
+    {
+        possibleTargets = FindObjectsOfType<Enemy>();
+        float minDistance = Mathf.Infinity;
+        Transform currTarget = null;
+        foreach(Enemy enemy in possibleTargets)
+        {
+            float currDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (currDistance < minDistance)
+            {
+                minDistance = currDistance;
+                currTarget = enemy.transform;
+            }
+        }
+        target = currTarget;
+    }
+    void Attack(bool isWithinRange)
+    {
+        var emissionModule = ballista.emission;
+        emissionModule.enabled = isWithinRange;
+    }
+
 }
